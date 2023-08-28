dbd25ffedfa9d45bc30a6efaccdb500055d75c41
lizz07 <119959581+lizz07@users.noreply.github.com>
2023-08-28 17:32:46 -0500

    Add files via upload

---
+++ flaskr/app.py
@@ -0,0 +1,22 @@
+from flaskr import create_app
+from .modelos import db,  Usuario, Album, Medio
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+# Prueba 1
+with app.app_context():
+    u = Usuario(nombre='juan', contrasena='12345')
+    a = Album(titulo='prueba', anio=1999, descripcion='texto', medio=Medio.CD)
+    u.albumes.append(a)
+    db.session.add(u)
+    db.session.commit()
+    print(Usuario.query.all())
+    print(Usuario.query.all()[0].albumes)
+    db.session.delete(u)
+    print(Usuario.query.all())
+    print(Album.query.all())
