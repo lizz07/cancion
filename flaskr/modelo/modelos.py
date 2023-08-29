c56fc4de1ab0b1774f96732949fa76b43890cc16
lizz07 <119959581+lizz07@users.noreply.github.com>
2023-08-29 13:48:35 -0500

    Update modelos.py

--- flaskr/modelo/modelos.py
+++ flaskr/modelo/modelos.py
@@ -2,14 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 import enum
 db = SQLAlchemy()
 
+
+class Medio(enum.Enum):
+    DISCO = 1
+    CASETE = 2
+    CD = 3
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    anio = db.Column(db.Integer)
+    descripcion = db.Column(db.String(128))
+    medio = db.Column(db.Enum(Medio))
+    usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    __table_args__ = (db.UniqueConstraint('usuario', 'titulo', name='titulo_unico_album'),)
+    canciones = db.relationship('Cancion',secondary='album_cancion', back_populates='id_Album')
+
 class Cancion(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String(128))
     minutos = db.Column(db.Integer)
     segundos = db.Column(db.Integer)
     interprete = db.Column(db.String(128))
-
-
+    id_Album = db.relationship('Album',secondary='album_cancion', back_populates='canciones')
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,19 +34,7 @@ class Usuario(db.Model):
 
 
 
-class Medio(enum.Enum):
-    DISCO = 1
-    CASETE = 2
-    CD = 3
 
-
-class Album(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(128))
-    anio = db.Column(db.Integer)
-    descripcion = db.Column(db.String(128))
-    medio = db.Column(db.Enum(Medio))
-    usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
-    __table_args__=(db.UniqueConstraint('usuario', 'titulo', name='titulo_unico_album'),)
-
-
+albumes_canciones = db.Table('album_cancion', \
+    db.Column('album_id',db.Integer, db.ForeignKey('album.id'), primary_key=True),\
+    db.Column('cancion_id',db.Integer, db.ForeignKey('cancion.id'), primary_key=True))
