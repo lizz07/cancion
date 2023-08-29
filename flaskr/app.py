b8b0055971eed93965584bb267b6524235825f79
lizz07 <119959581+lizz07@users.noreply.github.com>
2023-08-29 13:48:01 -0500

    Update app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db,  Usuario, Album, Medio
+from .modelos import db,  Usuario, Album, Medio , Cancion
 
 app = create_app('default')
 app_context = app.app_context()
@@ -10,8 +10,10 @@ db.create_all()
 
 # Prueba 1
 with app.app_context():
+    c = Cancion(titulo='Prueba', minutos=2, segundos=25, interprete='Juan Pablo')
     u = Usuario(nombre='juan', contrasena='12345')
     a = Album(titulo='prueba', anio=1999, descripcion='texto', medio=Medio.CD)
+    a.canciones.append(c)
     u.albumes.append(a)
     db.session.add(u)
     db.session.commit()
