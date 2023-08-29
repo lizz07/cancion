ed4ccad5d44af481cce39c5599833dec8ff81c6c
lizz07 <119959581+lizz07@users.noreply.github.com>
2023-08-29 17:43:31 -0500

    Update app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,7 @@
 from flaskr import create_app
-from .modelos import db, Cancion
+from .modelos import db, Cancion,Usuario,Album,Medio
 from flask_restful import Api
-from .vistas import vista_canciones , vista_cancion
+from .vistas import vista_canciones , vista_cancion , vista_usuarios , vista_usuario , vista_albumes, vista_album
 
 app = create_app('default')
 app_context = app.app_context()
@@ -14,3 +14,11 @@ api = Api(app)
 #ruta de la vista que quiero consultar el parametro la clase a usar y la ruta tiene tanto get como post
 api.add_resource(vista_canciones,'/canciones')
 api.add_resource(vista_cancion,'/canciones/<int:id>')
+
+#ruta usuario
+api.add_resource(vista_usuarios,'/usuarios')
+api.add_resource(vista_usuario,'/usuarios/<int:id>')
+
+# ruta album
+api.add_resource(vista_albumes,'/albumes')
+api.add_resource(vista_album,'/albumes/<int:id>')
